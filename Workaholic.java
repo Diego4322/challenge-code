fa8fd238722cd980cff43ed2535ec154708d4f85
Diego4322 <gldenpvp1@gmail.com>
2025-12-09 13:27:53 +0000

    Co-authored-by: SausySausages <SausySausages@users.noreply.github.com>

---
+++ Workaholic.java
@@ -0,0 +1,20 @@
+//hours worked: N (1 <N< 10°)
+//single integer 
+
+import java.util.Scanner;
+
+
+//we got lost
+
+
+public class Workaholic{
+    public static void main(String[] args){
+        java.util.Scanner.nextInt()
+        System.out.println("Enter work hours: ")
+        
+
+        
+    }
+    }
+}
+
